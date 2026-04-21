f4e2c734382874eb2a272d7a0eed73da365c024c
segin <segin>
2026-04-21 13:56:05 -0500

    Add unit tests for find/replace and line ending conversion

--- src/App/test/test_qwenpad.cpp
+++ src/App/test/test_qwenpad.cpp
@@ -74,6 +74,75 @@ void TestQwenpad::testConvertLineEndingsMixed()
     QCOMPARE(result, QString("Line1\r\nLine2\r\nLine3\r\nLine4"));
 }
 
+void TestQwenpad::testDocumentFind()
+{
+    QTextDocument doc;
+    doc.setPlainText("Hello World\nHello Qt\nHello World");
+
+    QTextCursor cursor(doc.find("Hello"));
+    QCOMPARE(cursor.selectedText(), QString("Hello"));
+
+    QTextCursor found = doc.find("Qt", cursor);
+    QVERIFY(!found.isNull());
+    QCOMPARE(found.selectedText(), QString("Qt"));
+}
+
+void TestQwenpad::testDocumentFindWrapAround()
+{
+    QTextDocument doc;
+    doc.setPlainText("Hello World\nHello Qt");
+
+    QTextCursor cursor(doc.find("Hello"));
+    QCOMPARE(cursor.selectedText(), QString("Hello"));
+
+    cursor.setPosition(0);
+    QTextCursor first = doc.find("World", cursor);
+    QVERIFY(!first.isNull());
+    QCOMPARE(first.selectedText(), QString("World"));
+}
+
+void TestQwenpad::testDocumentReplace()
+{
+    QTextDocument doc;
+    doc.setPlainText("Hello World");
+
+    QTextCursor cursor(doc);
+    cursor.beginEditBlock();
+
+    doc.find("World");
+    cursor = doc.find("World");
+    if (!cursor.isNull()) {
+        cursor.insertText("Qt");
+    }
+
+    cursor.endEditBlock();
+    QCOMPARE(doc.toPlainText(), QString("Hello Qt"));
+}
+
+void TestQwenpad::testDocumentReplaceEmpty()
+{
+    QTextDocument doc;
+    doc.setPlainText("Hello World Qt");
+
+    QTextCursor cursor(doc);
+    cursor.beginEditBlock();
+
+    while ((cursor = doc.find("Qt", cursor)) != QTextCursor()) {
+        cursor.removeSelectedText();
+        break;
+    }
+
+    cursor.endEditBlock();
+    QCOMPARE(doc.toPlainText(), QString("Hello World "));
+}
+
+void TestQwenpad::testConvertLineEndingsPreservesNewlines()
+{
+    Qwenpad app;
+    QString result = app.convertLineEndings("Hello\nWorld", 0);
+    QCOMPARE(result, QString("Hello\nWorld"));
+}
+
 #include "test_qwenpad.moc"
 
 QTEST_MAIN(TestQwenpad)
